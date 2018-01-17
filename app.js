b53860b4dc1018c4a8a0f4ae92e761fdb57009f9
Drew Disterhoft <drew.disterhoft@gmail.com>
2018-01-17 17:07:37 -0600

    Add state and UI
    Making our app logically constructed and easier to read.

--- app.js
+++ app.js
@@ -1,42 +1,51 @@
 
 function onReady() {
+  let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
+  const addToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
-  const deleteToDoForm = document.getElementById('deleteToDoForm')
 
-  addToDoForm.addEventListener('submit', () => {
-    event.preventDefault();
+  function createNewToDo() {
+    if (!newToDoText.value) { return }
 
-    //get the text
-    let title = newToDoText.value;
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
 
-    // create a new li
-    let newLi = document.createElement('li');
-
-    // create a new input
-    let checkbox = document.createElement('input');
-
-    // set the input's type to checkbox
-    checkbox.type = "checkbox";
-
-    //set the title
-    newLi.textContent = title;
-
-    // attach the checkbox to the Li
-    newLi.appendChild(checkbox);
-
-    // attach the li to the ul
-    toDoList.appendChild(newLi);
-
-    // empty the input
     newToDoText.value = '';
 
+    renderTheUI();
+  }
+
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo){
+      const newToDo = document.createElement('li');
+
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+
+      const title = document.createElement('span');
+      title.textContent = toDo.title;
+    });
+  }
+
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
+    newToDoText.value = '';
   });
 
-  deleteToDoForm.addEventListener('submit', () => {
-    let checkbox = document.deleteElement('input');
-  });
+  renderTheUI();
 }
 
 window.onload = function() {
